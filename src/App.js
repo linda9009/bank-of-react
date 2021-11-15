10d5a2bf7bf9b6bbb17b2b1535ef43a46e519093
linda9009 <14009556+linda9009@users.noreply.github.com>
2021-11-15 17:15:30 -0500

    added starting files

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home';
+import LogIn from './components/Login';
+import Debits from './components/Debits';
+// import Credits from './components/Credits';
+import UserProfile from './components/UserProfile';
+import axios from "axios";
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'joe_shmo',
+        memberSince: '07/23/96',
+      }
+    }
+
+  }
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
+  }
+  
+  render() {
+
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+        <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
+    );
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);
+
+    return (
+        <Router>
+          <div>
+            <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/userProfile" render={UserProfileComponent}/>
+            <Route exact path="/login" render={LogInComponent}/>
+            <Route exact path="/debits" render={DebitsComponent}/>  
+            <Route exact path="/credits" render={CreditComponent}/>
+            </div>
+        </Router>
+    );
+  }
+}
+
+export default App;
